056a5eb13c8a732da02c5a6f23f1c7862333ded7
Juliette RAYNAUD <juliette.raynaud@epsi.fr>
2021-10-05 16:54:01 +0200

    ajout emplacement pour els notes (plain text)

--- app/src/main/java/com/example/workshop/MainActivity.java
+++ app/src/main/java/com/example/workshop/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.workshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nom;
+    private EditText prenom;
+    private EditText numtel;
+    private EditText adresse;
+    private EditText notes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nom = (EditText) findViewById(R.id.nom);
+        prenom = (EditText) findViewById(R.id.prenom);
+        numtel = (EditText) findViewById(R.id.numtel);
+        adresse = (EditText) findViewById(R.id.adresse);
+        notes = (EditText) findViewById(R.id.notes);
+
+
+
+
+
+
+
+
     }
 }
