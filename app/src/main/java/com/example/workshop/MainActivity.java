8892f83d6c625d34dba1484bc3723c2bd9f56331
Gabrieldlb <gabriel.delabiche@epsi.fr>
2021-10-07 09:14:46 +0200

    couleur

--- app/src/main/java/com/example/workshop/MainActivity.java
+++ app/src/main/java/com/example/workshop/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.workshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLayout = (RelativeLayout) findViewById(R.id.layout);
+
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("Pref", 0 );
+
+
+        String tLayout = preferences.getString("Color", "");
+        mLayout.setBackgroundColor(Integer.parseInt(tLayout));
     }
 }
--- app/src/main/java/com/example/workshop/Settings.java
+++ app/src/main/java/com/example/workshop/Settings.java
@@ -1,14 +1,54 @@
 package com.example.workshop;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class Settings extends AppCompatActivity {
+
+    RelativeLayout mLayout;
+    int mDefaultColor = 0;
+    private Button color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        SharedPreferences preferences = getSharedPreferences("Pref", 0);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        mLayout = (RelativeLayout) findViewById(R.id.layout);
+        mDefaultColor = ContextCompat.getColor(Settings.this, R.color.white);
+        color = (Button) findViewById(R.id.color);
+
+        color.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openColorPicker(editor);
+            }
+        });
+    }
+
+    public void openColorPicker(SharedPreferences.Editor editor) {
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, mDefaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+            }
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                mDefaultColor = color;
+                mLayout.setBackgroundColor(mDefaultColor);
+                editor.putString("Color", String.valueOf(mDefaultColor));
+            }
+        });
+        colorPicker.show();
     }
 }
