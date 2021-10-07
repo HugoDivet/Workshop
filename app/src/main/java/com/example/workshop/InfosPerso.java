7cfae9743fc9858d773069424cb2e2ccee6a2c1b
Korgannon <Ascentia26@gmail.com>
2021-10-07 16:14:37 +0200

    LastCommit

--- app/src/main/java/com/example/workshop/InfosPerso.java
+++ app/src/main/java/com/example/workshop/InfosPerso.java
@@ -1,8 +1,11 @@
 package com.example.workshop;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class InfosPerso extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class InfosPerso extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infos_perso);
+
+        FloatingActionButton AddInfosPerso = (FloatingActionButton) findViewById(R.id.AddInfos);
+        FloatingActionButton SettingsInfos = (FloatingActionButton) findViewById(R.id.SettingsInfos);
+
+        AddInfosPerso.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent AddInfosPerso = new Intent(InfosPerso.this, AddInfosPerso.class);
+                startActivity(AddInfosPerso);
+            }
+        });
+        SettingsInfos.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent SettingsInfos = new Intent(InfosPerso.this, Settings.class);
+                startActivity(SettingsInfos);
+            }
+        });
     }
 }
